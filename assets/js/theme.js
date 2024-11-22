31d95d62bd501240c08d135a66ae6875d8490497
hafizadt ghazali <hafizadt.ghazali@gmail.com>
2024-11-22 12:38:19 +0800

    Add files via upload

---
+++ assets/js/theme.js
@@ -0,0 +1,386 @@
+(function ($) {
+    'use strict';
+
+    // Mobile Menu
+    $('.mobile-menu nav').meanmenu({
+        meanScreenWidth: "991",
+        meanMenuContainer: ".mobile-menu",
+        meanMenuOpen: "<span></span> <span></span> <span></span>",
+        onePage: false,
+    });
+
+
+        // =======< accordion js >========
+        $(".accordion > li:eq(0) a").addClass("active").next().slideDown();
+        $('.accordion a').on('click', function (j) {
+            var dropDown = $(this).closest("li").find("p");
+
+            $(this).closest(".accordion").find("p").not(dropDown).slideUp();
+
+            if ($(this).hasClass("active")) {
+                $(this).removeClass("active");
+            } else {
+                $(this).closest(".accordion").find("a.active").removeClass("active");
+                $(this).addClass("active");
+            }
+
+            dropDown.stop(false, true).slideToggle();
+
+            j.preventDefault();
+        });
+
+    // sticky
+    var wind = $(window);
+    var sticky = $('#sticky-header');
+    wind.on('scroll', function () {
+        var scroll = wind.scrollTop();
+        if (scroll < 100) {
+            sticky.removeClass('sticky');
+        } else {
+            sticky.addClass('sticky');
+        }
+    });
+    
+   //Header Search
+    if($('.search-box-outer').length) {
+        $('.search-box-outer').on('click', function() {
+            $('body').addClass('search-active');
+        });
+        $('.close-search').on('click', function() {
+            $('body').removeClass('search-active');
+        });
+    }
+
+    // Loder  //
+    $(function () {
+        $('body').addClass('loaded');
+    });
+     // Script nav
+    $(".team-share").click(function(){
+        $(this).siblings(".team-social-icon").toggleClass('active');
+    });
+
+     //======< scrollcue js >======
+    $(function(){
+          scrollCue.init({
+          duration : 2500,
+          interval : -0.7,
+          percentage : 0.90,
+          smartSpeed: 500 
+          
+        })
+    });
+
+
+    // category List
+    $('.project-list').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 10000,
+        dots: false,
+        nav: true,
+        navText: ["<i class='bi bi-arrow-left''></i>", "<i class='bi bi-arrow-right''></i>"],
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 2
+            },
+            1000: {
+                items: 2
+            },
+            1920: {
+                items: 3
+            }
+        }
+    })
+
+
+    // testimonial Active
+    $('.testimonial_list').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 9000,
+        dots:true,
+        nav: false,
+        navText: ["<i class='bi bi-arrow-left''></i>", "<i class='bi bi-arrow-right''></i>"],
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 1
+            },
+            992: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            },
+            1920: {
+                items: 1
+            }
+        }
+    })
+
+    // Brand Active
+    $('.brand-list').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 10000,
+        dots: false,
+        nav: false,
+        navText: ["<i class='fa fa-long-arrow-left''></i>", "<i class='fa fa-long-arrow-right''></i>"],
+        responsive: {
+            0: {
+                items: 1
+            },
+            480: {
+                items: 1
+            },
+            600:{
+                items:2
+            },
+            768: {
+                items: 3
+            },
+            992: {
+                items: 4
+            },
+            1000: {
+                items: 4
+            },
+            1920: {
+                items: 4
+            }
+        }
+    })
+
+    // testimonial Active
+    $('.brand-list-2').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 10000,
+        dots: false,
+        nav: false,
+        navText: ["<i class='fa fa-long-arrow-left''></i>", "<i class='fa fa-long-arrow-right''></i>"],
+        responsive: {
+            0: {
+                items: 2
+            },
+            400: {
+                items:3
+            },
+            600: {
+                items:3
+            },
+            768: {
+                items: 4
+            },
+            992: {
+                items: 4
+            },
+            1000: {
+                items: 5
+            },
+            1920: {
+                items: 5
+            }
+        }
+    })
+
+
+    /*---------------------
+    WOW active js 
+    --------------------- */
+    new WOW().init();
+
+    // counterUp
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+
+    /* Portfolio Isotope  */
+    $('.image_load').imagesLoaded(function () {
+
+        if ($.fn.isotope) {
+
+            var $portfolio = $('.image_load');
+
+            $portfolio.isotope({
+
+                itemSelector: '.grid-item',
+
+                filter: '*',
+
+                resizesContainer: true,
+
+                layoutMode: 'masonry',
+
+                transitionDuration: '0.8s'
+
+            });
+            $('.menu-filtering li').on('click', function () {
+
+                $('.menu-filtering li').removeClass('current_menu_item');
+
+                $(this).addClass('current_menu_item');
+
+                var selector = $(this).attr('data-filter');
+
+                $portfolio.isotope({
+
+                    filter: selector,
+
+                });
+
+            });
+
+        };
+
+    });
+
+    // Venubox
+
+    $('.venobox').venobox({
+
+        numeratio: true,
+
+        infinigall: true
+
+    });
+
+
+   // scroll up
+
+    if($('.prgoress_indicator path').length){
+        var progressPath = document.querySelector('.prgoress_indicator path');
+        var pathLength = progressPath.getTotalLength();
+        progressPath.style.transition = progressPath.style.WebkitTransition = 'none';
+        progressPath.style.strokeDasharray = pathLength + ' ' + pathLength;
+        progressPath.style.strokeDashoffset = pathLength;
+        progressPath.getBoundingClientRect();
+        progressPath.style.transition = progressPath.style.WebkitTransition = 'stroke-dashoffset 10ms linear';
+        var updateProgress = function () {
+          var scroll = $(window).scrollTop();
+          var height = $(document).height() - $(window).height();
+          var progress = pathLength - (scroll * pathLength / height);
+          progressPath.style.strokeDashoffset = progress;
+        }
+        updateProgress();
+        $(window).on('scroll', updateProgress);
+        var offset = 250;
+        var duration = 550;
+        jQuery(window).on('scroll', function () {
+          if (jQuery(this).scrollTop() > offset) {
+            jQuery('.prgoress_indicator').addClass('active-progress');
+          } else {
+            jQuery('.prgoress_indicator').removeClass('active-progress');
+          }
+        });
+        jQuery('.prgoress_indicator').on('click', function (event) {
+          event.preventDefault();
+          jQuery('html, body').animate({ scrollTop: 0 }, duration);
+          return false;
+        });
+    }
+
+
+    jQuery(document).ready(function ($) {
+        "use strict";
+
+        //=====< barfiller script >====
+        $('#bar1').barfiller({
+            duration: 7000
+        });
+        $('#bar2').barfiller({
+            duration: 7000
+        });
+        $('#bar3').barfiller({
+            duration: 7000
+        });
+
+         //======< sign Up >======
+        // Get the modal
+        var modal = document.getElementById('id01');
+
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+
+
+        //======< Custom Tab >======
+        $('.tab ul.tabs').addClass('active').find('> li:eq(0)').addClass('current');
+
+        $(".tab ul.tabs li a").on("click", function (g) {
+            var tab = $(this).closest('.tab'),
+                index = $(this).closest('li').index();
+
+            tab.find('ul.tabs > li').removeClass('current');
+            $(this).closest('li').addClass('current');
+
+            tab.find('.tab_content').find('div.tabs_item').not('div.tabs_item:eq(' + index + ')').slideUp();
+            tab.find('.tab_content').find('div.tabs_item:eq(' + index + ')').slideDown();
+
+            g.preventDefault();
+        });
+
+    });
+
+
+    // <!--jave script -->
+
+    $(window).on('scroll', function () {
+        var scrolled = $(window).scrollTop();
+        if (scrolled > 300) $('.go-top').addClass('active');
+        if (scrolled < 300) $('.go-top').removeClass('active');
+    });
+
+    $('.go-top').on('click', function () {
+        $("html, body").animate({
+            scrollTop: "0"
+        }, 1200);
+    });
+
+
+    $(".circle_percent").each(function () {
+        var $this = $(this),
+            $dataV = $this.data("percent"),
+            $dataDeg = $dataV * 3.6,
+            $round = $this.find(".round_per");
+        $round.css("transform", "rotate(" + parseInt($dataDeg + 180) + "deg)");
+        $this.append('<div class="circle_inbox"><span class="percent_text"></span></div>');
+        $this.prop('Counter', 0).animate({
+            Counter: $dataV
+        }, {
+            duration: 2000,
+            easing: 'swing',
+            step: function (now) {
+                $this.find(".percent_text").text(Math.ceil(now) + "%");
+            }
+        });
+        if ($dataV >= 51) {
+            $round.css("transform", "rotate(" + 360 + "deg)");
+            setTimeout(function () {
+                $this.addClass("percent_more");
+            }, 1000);
+            setTimeout(function () {
+                $round.css("transform", "rotate(" + parseInt($dataDeg + 180) + "deg)");
+            }, 1000);
+        }
+    });
+
+
+
+})(jQuery);
